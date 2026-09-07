2fb3cc2cc7571ba6de8ea14b266f2a013e6a5f87
RuslanAndruscha <ruslan.andruscha.dev@gmail.com>
2026-09-06 21:56:44 -0300

    Criando logs de servidor

--- servidor.py
+++ servidor.py
@@ -12,6 +12,7 @@ class TarefaServicer(tarefa_pb2_grpc.GerenciarTarefasServicer):
         #cria id, converte pra string e cria dicionario com os dados
         id_uuid = uuid.uuid4()
         id_str = str(id_uuid)
+        print('\nCriando tarefa ', id_str)
         #extrai dados da request
         tarefa = tarefa_pb2.Tarefa(
         id=id_str,
@@ -23,32 +24,39 @@ class TarefaServicer(tarefa_pb2_grpc.GerenciarTarefasServicer):
     )
         #salva tarefa
         self.bd_tarefas[id_str]=tarefa
-
+        print('Tarefa criada!')
         return tarefa
 
     def ListarTarefas(self, request, context):
         lista = tarefa_pb2.ResponseListarTarefas()
     # percorre os item do dicionario
+        print('\nListando tarefas...')
         for item in self.bd_tarefas.values():
             lista.tarefa.append(item)
         return lista
 
     def AtualizarTarefa(self, request, context):
         # ve se o id existe no dicionario
+        print(f'\nVerificando se tarefa {request.id} está no banco...')
         if request.id in self.bd_tarefas:
             # atualiza os dados da tarefa
             self.bd_tarefas[request.id] = request
+            print('Tarefa atualizada!')
             return request
         else:
             # se nao achar retorna vazio mesmo
+            print('Tarefa não encontrada! :(')
             return tarefa_pb2.Tarefa()
 
     def DeletarTarefa(self, request, context):
-                # tenta achar o id pra apagar
+        # tenta achar o id pra apagar
+        print(f'\nVerificando se tarefa {request.id} está no banco...')
         if request.id in self.bd_tarefas:
             del self.bd_tarefas[request.id]
+            print('\nTarefa deletada com sucesso!')
             return tarefa_pb2.ResponseDeletarTarefa(concluido=True)
         else:
+            print('\nTarefa não encontrada! :(')
             return tarefa_pb2.ResponseDeletarTarefa(concluido=False)
       
 
